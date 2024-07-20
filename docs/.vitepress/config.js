01a928fafa84f974d10972b073689b9cf722cdef
guoguohui <1345323795@qq.com>
2024-07-21 01:08:13 +0800

    feat:项目初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,59 @@
+import {nestjsSidebar} from "./backend/nestjsSiderbar";
+import {colorfulSidebar} from "./colorfulLife/colorfulSiderbar";
+import { nginxSidebar } from "./backend/nginxSidebar";
+import {bigManSidebar} from "./bigMan/bigManSiderbar";
+import {bugSidebar} from "./bug/bugSiderbar";
+import {frontendNav} from "./frontend";
+import {backendNav} from "./backend";
+import {gitSidebar} from "./backend/gitSidebar";
+ 
+
+export default {
+	base: '/hui_blog/',
+	outDir: '../public',
+	title: '疾风浴雪的花园',
+	description: 'Talk is cheap, show me your code.',
+	themeConfig: {
+		siteTitle: '疾风浴雪',
+		logo: '/img/moon.jpg',
+		nav: [
+			{
+				text: '首页',
+				link: '/index',
+			},
+			frontendNav,
+			backendNav,
+			{
+				text: '巨人的肩膀',
+				link: '/bigMan/',
+			},
+			{
+				text: 'bug集中营',
+				link: '/bug/',
+			},
+			{
+				text: '缤纷世界',
+				link: '/colorfulLife/',
+			},
+		],
+		sidebar: {
+			// 前端
+			// 后端
+			'/backend/nestjs/': nestjsSidebar,
+			'/backend/nginx/': nginxSidebar,
+			'/backend/gitLearn/': gitSidebar,
+			// 巨人的肩膀
+			'/bigMan/': bigManSidebar,
+			// bug集中营
+			'/bug/': bugSidebar,
+			// 缤纷世界
+			'/colorfulLife/': colorfulSidebar,
+		},
+
+		socialLinks: [
+			{icon: 'github', link: 'https://gitee.com/guohui.cn/hui-blog'},
+		]
+
+	}
+
+}
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+layout: home
+
+hero:
+  name: 疾风浴雪
+  text: Just Coding... 
+  tagline: Talk is cheap, show me your code!
+  image:
+    src: /img/index-read.gif
+    alt: VitePress
+  actions:
+    - theme: brand
+      text:  Welcome!
+      link: /colorfulLife/
+    - theme: alt
+      text: Gittee
+      link: https://gitee.com/guohui.cn/hui-blog
+features:
+  - icon: 🧑🏼‍💻‍
+    title: 内在丰盈者，独行亦如众
+    # details: 内在丰盈者，独行亦如众
+---
+
