47b712c572688edaea10640cb821d1796ad02439
guoguohui <1345323795@qq.com>
2024-07-21 10:54:44 +0800

    feat:deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,8 +9,8 @@ import {gitSidebar} from "./backend/gitSidebar";
  
 
 export default {
-	base: '/',
-	outDir: 'docs/dist',
+	base: '/hui_blog/',
+	outDir: 'dist',
 	title: '疾风浴雪的花园',
 	description: 'Talk is cheap, show me your code.',
 	themeConfig: {
